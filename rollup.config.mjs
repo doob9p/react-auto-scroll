659707df5ab27ba33cb60d654c0a476b1093ff42
doob9p <doob9p@gmail.com>
2022-12-05 17:34:05 +0900

    feat: rollup setting

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,33 @@
-import { babel } from "@rollup/plugin-babel";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
+import { babel } from "@rollup/plugin-babel"; // rollup과 babel 연동 플러그인
+import nodeResolve from "@rollup/plugin-node-resolve"; // node_modules에서 써드파티 모듈을 사용할 수 있게해주는 플러그인
+import commonjs from "@rollup/plugin-commonjs"; // commonjs 형태의 모듈을 es모듈로 변환해주는 플러그인
+import typescript from "rollup-plugin-typescript2"; // typescript 번들링 플러그인 (rollup-plugin-typescript 개선된 버전)
+import peerDepsExternal from "rollup-plugin-peer-deps-external"; // package.json에 명시된 peerDependency 모듈을 번들 결과에서 제외 시키는 모듈
+import postcss from "rollup-plugin-postcss"; // postcss 연동 플러그인
 import pkg from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: pkg.main,
-      format: "esm",
-      sourcemap: true,
+      dir: "./dist",
+      format: "cjs",
+      preserveModules: true,
+      preserveModulesRoot: "src",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+    {
+      name: pkg.name,
+      file: pkg.browser,
+      format: "umd",
     },
   ],
   plugins: [
     commonjs(),
+    peerDepsExternal(),
     nodeResolve(),
     typescript({
       useTsconfigDeclarationDir: true,
@@ -27,10 +39,6 @@ export default {
       include: "src/**/*.(ts|tsx|js|jsx)",
       extensions: [".ts", ".tsx", ".js", ".jsx", ".es", ".es6", ".mjs"],
     }),
-    postcss({
-      extract: false,
-      modules: true,
-      sourceMap: false,
-    }),
+    postcss(),
   ],
 };
--- src/components/ReactAutoScroll/ReactAutoScroll.tsx
+++ src/components/ReactAutoScroll/ReactAutoScroll.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./ReactAutoScroll.css";
 import useInterval from "../../hooks/useInterval";
 
